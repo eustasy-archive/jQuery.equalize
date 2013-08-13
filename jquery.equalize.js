f6a572b7d14f90c6c58cdae84aed050c1d36230d
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-08-13 23:51:25 +0100

    Expanded jquery.equalize.js
    Added Version Number
    Added Links
    Added 'use strict'; to all functions

--- jquery.equalize.js
+++ jquery.equalize.js
@@ -1,8 +1,12 @@
 ////////////////////////////////////////////////////////
 ////////                                        ////////
 ////////            jQuery Equalize             ////////
+////////                 v.1.1                  ////////
 ////////                                        ////////
 ////////////////////////////////////////////////////////
+//                                                    //
+//  http://labs.eustasy.org/jquery.equalize/          //
+//  https://github.com/eustasy/jquery.equalize        //
 //                                                    //
 //  This small jQuery Script equalizes the height of  //
 //  all the divs with the class of equalize within    //
@@ -15,15 +19,25 @@
 ////////////////////////////////////////////////////////
 
 function equalize() { // Define a new function
+    'use strict'; // Be strict
     $('.group').each(function(){ // For each group class
         var highestBox = 0; // Clear the highest height
-        $('.equalize', this).css('height', 'auto') // Set all the equalize classes to auto
+        $('.equalize', this).css('height', 'auto'); // Set all the equalize classes to auto
         $('.equalize', this).each(function(){ // For each equalize class
-            // If it's height is bigger than the highest height, it's the new heighest
-            if($(this).height() > highestBox) highestBox = $(this).height();
+            if($(this).height() > highestBox) { // If it's height is bigger than the highest height
+                highestBox = $(this).height(); // it's the new heighest
+            }
         }); // Finshed with this group?
         $('.equalize',this).height(highestBox); // Set them to all be the heighest
     }); // Start again, or finished with all the groups?
 } // Finish defining the function
-$(function(){ equalize(); }); // Run the function when the page has loaded
-window.onresize = function() { equalize(); } // And every time the window is resized.
+
+$(function(){ // When the page has loaded
+    'use strict';  // You should always be strict
+    equalize(); // Run the function 
+}); 
+
+window.onresize = function() { // And every time the window is resized
+    'use strict'; // Always
+    equalize(); // Here we go again
+}; 
